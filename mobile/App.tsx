24f853e33e22423464480c78b73935859f6d4940
Paulo Santana <paulosantana.sa@gmail.com>
2022-09-18 11:04:44 -0300

    NLW - Power up: Adjusted home page app and created Game Screen and DuoCards components.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,9 +7,9 @@ import {
   Inter_900Black
 } from '@expo-google-fonts/inter'
 
-import { Background } from './src/components/Background';
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
 import { Loading } from './src/components/Loading';
+import { Background } from './src/components/Background';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -27,7 +27,7 @@ export default function App() {
       backgroundColor="transparent"
       translucent
       />
-      {fontsLoaded ? <Home /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
 
     </Background>
   );
--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -5,10 +5,12 @@ import { styles } from './styles';
 import { THEME } from '../../theme';
 
 export interface GameCardProps {
-  id: String;
-  name: String;
-  ads: String;
-  cover: ImageSourcePropType;
+  id: string;
+  title: string;
+  _count: {
+    ads: number
+  }
+  bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps {
@@ -20,18 +22,18 @@ export function GameCard({ data, ...rest }: Props) {
     <TouchableOpacity style={styles.container} {...rest}>
       <ImageBackground
       style={styles.cover}
-      source={data.cover}
+      source={{ uri: data.bannerUrl}}
       >
         <LinearGradient
         colors={THEME.COLORS.FOOTER}
         style={styles.footer}
         >
-          <Text style={styles.name}>
-            {data.name}
+          <Text style={styles.title}>
+            {data.title}
           </Text>
           
           <Text style={styles.ads}>
-            {data.ads} anúncios
+            {data._count.ads} anúncios
           </Text>
 
         </LinearGradient>
